f12e5e4b469b7776d49c5cbfd9040a93ee948382
Your Name <you@example.com>
2020-02-26 12:03:13 -0600

    Starting forloops in Array-js

---
+++ arrays.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+// 1. printIndices
+function printIndices(items) { 
+  
+  // Replace this with your code
+}
+
+
+
+// 2. everyOtherItem
+function everyOtherItem(items) {
+  // Replace this with your code
+}
+
+
+// 3. smallestNItems
+function smallestNItems(items, n) {
+  // Replace this with your code
+}
+
+const fruits = ['apple', 'berry', 'cherry'];
+printIndices(fruits);
